3ee0eb015915d53ceb5e05bf61c5e457bb9fde6f
Matthew Cheverie <matthewjohncheverie@gmail.com>
2022-09-18 01:46:56 -0400

    Starter files aswell as initial print statements

---
+++ LogisticsSim/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// Main
+public class Main {
+    // main() class
+    public static void main(String[] args) {
+
+        // using Scanner to get in put from user
+        Scanner user = new Scanner(System.in);
+
+        //Prompts user about inputs they can use
+        System.out.println("Welcome to Logistics Sims");
+        System.out.println("Are you a 1. Customer or 2. Employee");
+        System.out.println("Please enter corresponding number to continue");
+
+        /*
+         * Switch Statement prompting User("Customer") or Admin("Employee") of function
+         * they like to use including run simulation
+         */
+
+
+    } // end main()class
+}
